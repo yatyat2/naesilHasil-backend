950a9ba2e63b7fc9a305bd0ec3a6b14a55e5189b
yatyat2 <xowls2287@naver.com>
2021-05-31 19:51:32 +0900

    상수 파일 추가

---
+++ src/constant.ts
@@ -0,0 +1,9 @@
+const SERVER_PORT = 4000;
+
+const DB_PORT = 3306;
+const DB_HOST = '127.0.0.1';
+const DB_USER = 'root';
+const DB_PASSWORD = '4858952287tj';
+const DB_NAME = 'naesilHashil';
+
+export { DB_PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, SERVER_PORT };
